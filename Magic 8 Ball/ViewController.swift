073508fbfb71ec36488e3c2636a4fbee9ac0a87a
dbae82 <dbae82@gmail.com>
2022-03-22 16:57:13 -0500

    made ball image random with image array

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"), #imageLiteral(resourceName: "ball2.png"), #imageLiteral(resourceName: "ball3.png"), #imageLiteral(resourceName: "ball4.png"), #imageLiteral(resourceName: "ball5.png")]
     
     @IBOutlet weak var ballImage: UIImageView!
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
         
-        ballImage.image = #imageLiteral(resourceName: "ball3")
+        ballImage.image = ballArray.randomElement()
         
     }
     
